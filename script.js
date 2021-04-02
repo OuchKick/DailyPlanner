8ce678ccc9249205e6ea9472e7f5573d8d0fe55c
Adam Reed <thirdeyeadam@gmail.com>
2021-04-01 19:53:22 -0500

    additional update to JS

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var times = [10,11,12,13,14,15,16,17,18];
 var timesSecond = ["10:00:00", "11:00:00", "12:00:00", "13:00:00",  "14:00:00",  "15:00:00",  "16:00:00",  "17:00:00",  "18:00:00"];
 
 var mainCont = [];
-var localStorage = JSON.parse(localStorage.getItem("workDay"));
-var buttonEl = document.getElementsByClassName('saveBtn');
+var theLocalStorage = JSON.parse(localStorage.getItem("workDay"));
+var buttonEl = $('.saveBtn');
 
 
 
@@ -16,17 +16,22 @@ console.log(hour)
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 
-buttonEl.addEventListener("click", function(){
+$('.saveBtn').click(function(event){
+    console.log(event.target.name)
+    var inputId ="#"+event.target.name;
+    console.log(inputId)
+    console.log($(inputId).val())
+    localStorage.setItem("workDay", JSON.stringify(inputId));
+    
+    
+    
 
+})
 
-
-
-
-});
 
 for (var i = 0; i < idTimes.length; i++) {
     var inputEl = $(idTimes[i]);
-    var buttonEl = document.getElementsByClassName('saveBtn');
+    var buttonEl = $('.saveBtn');
      console.log(times[i], hour)
     if (times[i] < hour) {
         inputEl.attr("class", "past");
@@ -40,20 +45,6 @@ for (var i = 0; i < idTimes.length; i++) {
    
 };
 
-// $('saveBtn').on("click", function(){
-//   var container = $(this).parent().parent();  
-//   var inputValue = container.find("input").val();
-//   var inputId = container.find("input").attr("id");
-//   var textObj = {
-//     "input-id": inputId,
-//     "input-value": inputValue };
-    
-//     if (textObj["input-value"] !== "") {
-//         plannerContent.push(textObj);
-//     localStorage.setItem("workDay", JSON.stringify(mainCont));
-//     }
-
-// });
 
 
 
