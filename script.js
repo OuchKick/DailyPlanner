935e2d3cd16de81db246f2d5d4ae3f1dad178bfb
Adam Reed <thirdeyeadam@gmail.com>
2021-04-01 20:47:43 -0500

    Unable to complete assignment

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var times = [10,11,12,13,14,15,16,17,18];
 var timesSecond = ["10:00:00", "11:00:00", "12:00:00", "13:00:00",  "14:00:00",  "15:00:00",  "16:00:00",  "17:00:00",  "18:00:00"];
 
 var mainCont = [];
-var theLocalStorage = JSON.parse(localStorage.getItem("workDay"));
 var buttonEl = $('.saveBtn');
+
 
 
 
@@ -16,16 +16,13 @@ console.log(hour)
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 
+
 $('.saveBtn').click(function(event){
     console.log(event.target.name)
     var inputId ="#"+event.target.name;
     console.log(inputId)
     console.log($(inputId).val())
-    localStorage.setItem("workDay", JSON.stringify(inputId));
-    
-    
-    
-
+    localStorage.setItem($(inputId).val(), JSON.stringify(inputId));
 })
 
 
@@ -44,6 +41,27 @@ for (var i = 0; i < idTimes.length; i++) {
 
    
 };
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+
+$('#hour-1 .description').val(localStorage.getItem('hour-1'));
+
+$('#hour-2 .description').val(localStorage.getItem('hour-2'));
+
+$('#hour-3 .description').val(localStorage.getItem('hour-3'));
+
+$('#hour-4 .description').val(localStorage.getItem('hour-4'));
+
+$('#hour-5 .description').val(localStorage.getItem('hour-5'));
+
+
+
 
 
 
